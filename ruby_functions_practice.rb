8fb314b14d8b4d5f89807b6b79053b522dacc9d1
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-07-16 15:40:22 +0100

    test return 10 successful

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,45 @@
+#1
+def return_10
+  return 10
+end
+
+# #2
+# def add(first_number, second_number)
+#   return first_number + second_number
+# end
+
+# #3
+# def subtract(first_number, second_number)
+#   return first_number - second_number
+# end
+
+# #4
+# def multiply(first_number, second_number)
+#   return first_number * second_number
+# end
+
+# #5
+# def divide(first_number, second_number)
+#   return first_number / second_number
+# end
+
+# #6
+# def length_of_string(string)
+#   return string.length
+# end
+
+# #7
+# def join_string(string_1, string_2)
+#   return string_1 + string_2
+# end
+
+# #8
+# def add_string_as_number(number_1, number_2)
+#   return number_1.to_i + number_2.to_i
+# end
+
+#9
+def number_to_full_name__month_1(number_to_month)
+  return result = number_to_full_month_name( 1 )
+  assert_equal( "January", result )
+end
