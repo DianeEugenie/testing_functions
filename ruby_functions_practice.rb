b30ba9e97957a1cb4286e92bd2fbe585a9618fde
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-07-16 15:46:00 +0100

    test joing string sucessful

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -23,23 +23,23 @@
 #   return first_number / second_number
 # end
 
-#6
-def length_of_string(string)
-  return string.length
-end
-
-# #7
-# def join_string(string_1, string_2)
-#   return string_1 + string_2
+# #6
+# def length_of_string(string)
+#   return string.length
 # end
+
+#7
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
 
 # #8
 # def add_string_as_number(number_1, number_2)
 #   return number_1.to_i + number_2.to_i
 # end
 
-#9
-def number_to_full_name__month_1(number_to_month)
-  return result = number_to_full_month_name( 1 )
-  assert_equal( "January", result )
-end
+# #9
+# def number_to_full_name__month_1(number_to_month)
+#   return result = number_to_full_month_name( 1 )
+#   assert_equal( "January", result )
+# end
