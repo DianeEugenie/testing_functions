82d34826d33bab71595f3c07b56abde5d756b1da
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-07-16 17:48:08 +0100

    Last check to run all tests at the same time - 17 runs and 17 assertions

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,79 +1,79 @@
-# #1
-# def return_10
-#   return 10
-# end
+#1
+def return_10
+  return 10
+end
 
-# #2
-# def add(first_number, second_number)
-#   return first_number + second_number
-# end
+#2
+def add(first_number, second_number)
+  return first_number + second_number
+end
 
-# #3
-# def subtract(first_number, second_number)
-#   return first_number - second_number
-# end
+#3
+def subtract(first_number, second_number)
+  return first_number - second_number
+end
 
-# #4
-# def multiply(first_number, second_number)
-#   return first_number * second_number
-# end
+#4
+def multiply(first_number, second_number)
+  return first_number * second_number
+end
 
-# #5
-# def divide(first_number, second_number)
-#   return first_number / second_number
-# end
+#5
+def divide(first_number, second_number)
+  return first_number / second_number
+end
 
-# #6
-# def length_of_string(string)
-#   return string.length
-# end
+#6
+def length_of_string(string)
+  return string.length
+end
 
-# #7
-# def join_string(string_1, string_2)
-#   return string_1 + string_2
-# end
+#7
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
 
-# #8
-# def add_string_as_number(number_1, number_2)
-#   return number_1.to_i + number_2.to_i
-# end
+#8
+def add_string_as_number(number_1, number_2)
+  return number_1.to_i + number_2.to_i
+end
 
-# #9
-# def number_to_full_month_name(number)
-#   case number
-#     when 1
-#       return "January"
-#     when 3
-#       return "March"
-#     when 9
-#       return "September"
-#   end
-# end
+#9
+def number_to_full_month_name(number)
+  case number
+    when 1
+      return "January"
+    when 3
+      return "March"
+    when 9
+      return "September"
+  end
+end
 
-# #10
-# def number_to_short_month_name(number)
-#   case number
-#     when 1
-#       return "Jan"
-#     when 4
-#       return "Apr"
-#     when 10
-#       return "Oct"
-#   end
-# end
+#10
+def number_to_short_month_name(number)
+  case number
+    when 1
+      return "Jan"
+    when 4
+      return "Apr"
+    when 10
+      return "Oct"
+  end
+end
 
-# #11
-# def volume_of_cube(length)
-#
-#   return length ** 3
-#
-# end
+#11
+def volume_of_cube(length)
 
-# #12
-# def volume_of_sphere(radius)
-#   $pi = 3.14159265359
-#   return radius ** 3 * 4/3 * $pi
-# end
+  return length ** 3
+
+end
+
+#12
+def volume_of_sphere(radius)
+  $pi = 3.14159265359
+  return radius ** 3 * 4/3 * $pi
+end
 
 
 #13
@@ -82,5 +82,3 @@ def fahrenheit_to_celsius(fahrenheit)
   return (fahrenheit - 32) * 5/9
 
 end
-
-# p fahrenheit_to_celsius(25) = comes to -4 (rounded down)
